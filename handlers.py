de4a8e306bfc8b9a8151ddcbb29d38eeb05c783a
Alexander Sergunin <d1str@yandex.ru>
2024-11-08 00:12:07 +1000

    ver0.2

--- handlers.py
+++ handlers.py
@@ -1,17 +1,33 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 from aiogram.filters import CommandStart, Command
+
 
 import keyboards as kb
 
 router = Router()
 
-
+@router.callback_query(F.data == 'back')
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Добро пожаловать, выберете действие: ",
-                         reply_markup=kb.main)
+    if isinstance(message, Message):
+        await message.answer("Добро пожаловать, выберете действие: ",
+                            reply_markup=kb.main)
+    else:
+        await message.answer('')
+        await message.message.delete()
+        await message.message.answer("Добро пожаловать, выберете действие: ",
+                            reply_markup=kb.main)
 
-@router.message(F.text == "Добавление сотрудника")
-async def main_menu(message: Message):
-    await message.answer('Функция в разработке!...')
+
+
+@router.message(F.text == 'Структура компании')
+async def company_department(message: Message):
+    await message.answer('Выберете отдел: ',
+                         reply_markup=await kb.company_department_builder())
+
+@router.callback_query(F.data.startswith('dep_'))
+async def company_department_show(callback: CallbackQuery):
+    await callback.answer('Вы выбрали пункт')
+    await callback.message.answer(f'Вы выбрали {callback.data}',
+                                  reply_markup=ReplyKeyboardRemove())
--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,30 @@
 from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
                            InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-
-main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Добавление сотрудника', callback_data='add_worker'),
-     InlineKeyboardButton(text='Добавление отдела', callback_data='add_department')],
-    [InlineKeyboardButton(text='Структура компании', callback_data='company')],
-    [InlineKeyboardButton(text='Увольнение сотрудника', callback_data='remove_worker')],
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Добавление сотрудника'),
+     KeyboardButton(text='Добавление отдела')],
+    [KeyboardButton(text='Структура компании')],
+    [KeyboardButton(text='Увольнение сотрудника')],
 ])
+
+company_department = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Директорат', callback_data='dep_Директорат')],
+    [InlineKeyboardButton(text='Юристы', callback_data='dep_Юристы')],
+    [InlineKeyboardButton(text='Бухгалтерия', callback_data='dep_Бухгалтерия')],
+    [InlineKeyboardButton(text='ПЕО', callback_data='dep_ПЕО')],
+    [InlineKeyboardButton(text='Менеджеры', callback_data='dep_Менеджеры')],
+    [InlineKeyboardButton(text='ПТО', callback_data='dep_ПТО')],
+    [InlineKeyboardButton(text='МТО', callback_data='dep_МТО')],
+    [InlineKeyboardButton(text='IT', callback_data='dep_IT')],
+])
+
+async def company_department_builder():
+    keyboard = InlineKeyboardBuilder()
+    departments = {1: "Directorate", 2: "Uristi", 3: "Buh", 4: "Peo", 5: "Manager", 6:"PTO", 7: "MTO", 8: "IT"}
+    for department_id, department in departments.items():
+        keyboard.add(InlineKeyboardButton(text=department, callback_data=f'dep_{department_id}'))
+    keyboard.add(InlineKeyboardButton(text='На главную', callback_data=f'back'))
+    return keyboard.adjust(2).as_markup()
